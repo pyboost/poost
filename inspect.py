064348b869e8432d7945e5e6f9e482c65a494ba8
Pengkui Luo <pengkui.luo@gmail.com>
2013-04-20 18:28:06 -0500

    Remove _utils in naming.

---
+++ inspect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# CONTRIBUTORS (sorted by surname)
+# LUO, Pengkui <pengkui.luo@gmail.com>
+#
+#
+# UPDATED ON
+# 2012: 11/17,
+# 2013:
+#
+"""
+
+"""
