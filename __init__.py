b6a6d32c05e5fefd3127dbfe26953563ae51ec50
Pengkui Luo <pengkui.luo@gmail.com>
2013-06-19 17:54:08 -0700

    Refactor 'inet' out.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,5 @@
 # Classes or utilities that should be brought to the top-level namespace.
-from containers import *
 
 # Utilities that are supposed to use with another layer of namespace
-import inet
+#import .inet
 
-try:
-    del containers
-except NameError:
-    pass
